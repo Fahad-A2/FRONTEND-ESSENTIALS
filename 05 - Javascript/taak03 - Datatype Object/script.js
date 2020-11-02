441dd2fed7612a6b9ad0160143f39d166a9b4580
Fahad Khokhar <fahadabbasi2002@gmail.com>
2020-11-02 14:57:45 +0100

    -

---
+++ 05 - Javascript/taak03 - Datatype Object/script.js
@@ -0,0 +1,8 @@
+let student = {
+    voornaam :'Fahad',
+    achternaam : 'Abbasi',
+    leeftijd : 18,
+    ikBenStemGerechtig : true,
+    ikMagLegaalAlcoholKopen : true,
+    ikMagBrommerRijden : true
+}

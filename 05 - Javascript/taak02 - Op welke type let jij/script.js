6bf0c5024244a80084434b6a6009d242c56e3245
Fahad Khokhar <fahadabbasi2002@gmail.com>
2020-11-02 14:50:47 +0100

    -

---
+++ 05 - Javascript/taak02 - Op welke type let jij/script.js
@@ -0,0 +1,7 @@
+let voornaam = 'Fahad';
+let achternaam = 'Abbasi';
+let leeftijd = 18;
+let ikBenStemGerechtig = true;
+let ikMagLegaalAlcoholKopen = true;
+let ikMagBrommerRijden = true;
+let SoftwareOntwikkeling = true;

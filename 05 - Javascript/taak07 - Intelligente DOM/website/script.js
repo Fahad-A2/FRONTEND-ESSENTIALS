b4f7a255b8ea1c67823c05ee0933d1e898d328f0
Fahad Khokhar <fahadabbasi2002@gmail.com>
2020-11-04 11:33:51 +0100

    -

--- 05 - Javascript/taak07 - Intelligente DOM/website/script.js
+++ 05 - Javascript/taak07 - Intelligente DOM/website/script.js
@@ -6,4 +6,10 @@ console.log(window.document);
 // document.querySelector("#posterimg1").src = null;
 
 
-// document.querySelector("#poster1");
+let img1 = document.querySelector("#poster1");
+let img2 = document.querySelector("#poster2");
+let img3 = document.querySelector("#poster3");
+
+img1.style.backgroundColor = 'red'
+img2.style.backgroundColor = 'red'
+img3.style.backgroundColor = 'red'
